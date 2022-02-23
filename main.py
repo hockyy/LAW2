8e345d02328c6289bec3fb38795749bba5bc24dd
Hocky Yudhiono <hocky.yudhiono@gmail.com>
2022-02-23 20:54:35 +0700

    Add files

--- main.py
+++ main.py
@@ -2,25 +2,23 @@ from typing import Optional
 
 from fastapi import FastAPI, Form
 
-from model import MathQuery
+from model import MathQuery, MessageResponse, ValueResponse, MathResponse
 
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/", response_model=MessageResponse)
 async def read_root():
-    return {"Hello": "World"}
+    res = MessageResponse("Hello World", 200)
+    print("HELLO")
+    return res
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
-
-@app.get("/power/{a}/{b}")
+@app.get("/power/{a}/{b}", response_model=ValueResponse)
 async def read_power(a: int, b: int):
     '''
     Latihan pertama, menentukan a pangkat b dengan kedua parameter ialah bilangan bulat
     '''
-    return {"result":a**b}
+    return ValueResponse(a**b, 200)
 
 def generate_password(username: str):
     '''
@@ -30,20 +28,20 @@ def generate_password(username: str):
     '''
     return username[::-1]
 
-@app.post("/login")
+@app.post("/login", response_model=MessageResponse)
 async def login(username: str = Form(...), password: str = Form(...)):
     '''
     Latihan kedua, fungsi login yang akan mengauthorize jika dan hanya jika username[::-1] = password
     '''
-    return {"authorized": generate_password(username) == password}
+    return MessageResponse("True" if generate_password(username) == password else "False", 200)
 
-@app.post("/modulo")
+@app.post("/modulo", response_model=MathResponse)
 async def read_modulo(mathQuery: MathQuery):
     '''
     Melakukan modulo untuk bilangan pertama dan kedua, format masukan berupa sebuah payload dengan parameter a dan b sebagai kedua bilangan bulat
     '''
     try:
-        return {"query": mathQuery, "result": mathQuery.a%mathQuery.b}
+        return MathResponse(mathQuery, mathQuery.a%mathQuery.b, None)
     except Exception as e:
-        return {"query": mathQuery, "error": str(e)}
+        return MathResponse(mathQuery, mathQuery.a%mathQuery.b, str(e))
 
--- model.py
+++ model.py
@@ -1,6 +1,35 @@
+from typing import Any, Optional
+
 from pydantic import BaseModel
 
 
 class MathQuery(BaseModel):
     a: int
     b: int
+
+class MessageResponse(BaseModel):
+
+    def __init__(self, msg: str, code: int, **data: Any):
+        super().__init__(**data)
+        self.msg = msg
+        self.code = code
+
+    msg: Optional[str] = None
+    code: Optional[int] = 0
+
+class ValueResponse(BaseModel):
+    def __init__(self, msg: int, code: int, **data: Any):
+        super().__init__(**data)
+        self.msg = msg
+        self.code = code
+    msg: Optional[int] = 0
+    code: Optional[int] = 0
+
+class MathResponse(BaseModel):
+    def __init__(self, query: MathQuery, result: int, err : str, **data: Any):
+        super().__init__(**data)
+        self.query = query
+        self.result = result
+        self.err = err
+    query: Optional[MathQuery] = None
+    result: Optional[int] = 0
